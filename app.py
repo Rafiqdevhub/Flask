ac58338dcb5149d8621645d247c0d87aa0c32a7f
MUhammad Rafiq <rafiq85825@gmail.com>
2025-02-01 14:53:24 +0500

    Make email field unique in UserModel to prevent duplicate entries

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ api=Api(app)
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50),unique=True, nullable=False)
 
     def __repr__(self):
         return f"User(name={self.name}, email={self.email})"
